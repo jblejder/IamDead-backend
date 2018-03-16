3e7ac1861cec49d9045718c88bd7ad1d94f4dc57
jblejder <jakubblejder@gmail.com>
2018-03-16 22:09:59 +0100

    add bodyparser

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,16 @@
 import * as express from "express";
+import * as bodyParser from "body-parser"
 import * as batteryStats from "./controllers/batteryStats";
 
+
 const app = express();
+
+app.use(bodyParser.json())
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
 });
 app.get("/stats", batteryStats.getStat);
+app.post("/stats", batteryStats.postStat);
 
 export default app;
--- src/controllers/batteryStats.ts
+++ src/controllers/batteryStats.ts
@@ -3,3 +3,7 @@ import { Request, Response } from "express";
 export let getStat = (req: Request, res: Response) => {
     res.send("here will be stats");
 };
+
+export let postStat = (req: Request, res: Response) => {
+    res.send(req.body);
+};
